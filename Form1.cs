92c33aba763094ca67896a8379e99e01a08ce333
yimingliu-pku <58380633+yimingliu-pku@users.noreply.github.com>
2020-12-01 10:17:38 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 
 namespace gpa_calculator
 {
-    public partial class Form1 : Form
+   public partial class Form1 : Form
     {
         public Form1()
         {
@@ -21,6 +21,7 @@ namespace gpa_calculator
             cmbType.Items.Add("分数转换为GPA");
             cmbType.Items.Add("GPA转换为分数");
             cmbType.SelectedIndex = 0;
+
         }
 
         private void btnTansfer_Click(object sender, EventArgs e)
@@ -30,20 +31,29 @@ namespace gpa_calculator
                 if (0 == cmbType.SelectedIndex)
                 {
                     string transferedgpa = "";
-                    double grade = Convert.ToDouble(txtGPAorGrade.Text);
-                    if (grade >= 60.0 && grade <= 100.0)
+                    try
                     {
-                        transferedgpa = (4.0 - 3.0 * (100 - grade) * (100 - grade) / 1600).ToString("#0.00");
-                        MessageBox.Show("分数对应的GPA为" + transferedgpa);
+                        double grade = Convert.ToDouble(txtGPAorGrade.Text);
+
+                        if (grade >= 60.0 && grade <= 100.0)
+                        {
+                            transferedgpa = (4.0 - 3.0 * (100 - grade) * (100 - grade) / 1600).ToString("#0.00");
+                            MessageBox.Show("分数对应的GPA为" + transferedgpa);
+                        }
+                        else if (grade <= 60.0 && grade >= 0.0)
+                        {
+                            transferedgpa = "0";
+                            MessageBox.Show("分数对应的GPA为" + transferedgpa);
+                        }
+                        else
+                        {
+                            MessageBox.Show("输入错误，请重新输入");
+                            btnClear_Click(sender, e);
+                        }
                     }
-                    else if (grade <= 60.0 && grade >= 0.0)
+                    catch (Exception ex)
                     {
-                        transferedgpa = "0";
-                        MessageBox.Show("分数对应的GPA为" + transferedgpa);
-                    }
-                    else
-                    {
-                        MessageBox.Show("输入错误，请重新输入");
+                        MessageBox.Show(ex.Message);
                         btnClear_Click(sender, e);
                     }
 
@@ -51,20 +61,28 @@ namespace gpa_calculator
                 else
                 {
                     string transferedgrade = "";
-                    double gpa = Convert.ToDouble(txtGPAorGrade.Text);
-                    if (gpa <= 4.0 && gpa >= 1.0)
+                    try
                     {
-                        transferedgrade = (100.0 - Math.Sqrt((4.0 - gpa) * 1600 / 3)).ToString("#0.00");
-                        MessageBox.Show("GPA对应的分数为" + transferedgrade);
+                        double gpa = Convert.ToDouble(txtGPAorGrade.Text);
+                        if (gpa <= 4.0 && gpa >= 1.0)
+                        {
+                            transferedgrade = (100.0 - Math.Sqrt((4.0 - gpa) * 1600 / 3)).ToString("#0.00");
+                            MessageBox.Show("GPA对应的分数为" + transferedgrade);
+                        }
+                        else if (0.0 == gpa)
+                        {
+                            transferedgrade = "不及格（小于60分）";
+                            MessageBox.Show("GPA对应的分数为" + transferedgrade);
+                        }
+                        else
+                        {
+                            MessageBox.Show("输入错误，请重新输入");
+                            btnClear_Click(sender, e);
+                        }
                     }
-                    else if (0.0 == gpa)
+                    catch (Exception ex)
                     {
-                        transferedgrade = "不及格（小于60分）";
-                        MessageBox.Show("GPA对应的分数为" + transferedgrade);
-                    }
-                    else
-                    {
-                        MessageBox.Show("输入错误，请重新输入");
+                        MessageBox.Show(ex.Message);
                         btnClear_Click(sender, e);
                     }
                 }
@@ -110,55 +128,63 @@ namespace gpa_calculator
             goal.Text = txtNameGoal.Text + "想要达到的总绩点与这学期绩点的对应关系";
             if (txtNameGoal.Text!="" && txtTempCredit.Text!="" && txtTempGPA.Text != ""&& txtThisCredit.Text != "")
             {
-                double origingpa = Convert.ToDouble(txtTempGPA.Text);
-                double thissemcredit = Convert.ToDouble(txtThisCredit.Text);
-                double origincredit = Convert.ToDouble(txtTempCredit.Text);
-                string name = txtNameGoal.Text;
-                goal.dataGridView1.Columns.Add("TotalGPA", name + "想要达到的总绩点");
-                goal.dataGridView1.Columns.Add("ThisSemGPA", name + "这学期需要得到的绩点");
-                goal.dataGridView1.Columns.Add("ThisSemGrade", name + "这学期需要得到的成绩");
-                if (txtGoalGPA.Text!="")
+                try
                 {
-                    if (Convert.ToDouble(txtGoalGPA.Text) < 4.0 && Convert.ToDouble(txtGoalGPA.Text) > 1.0)
+                    double origingpa = Convert.ToDouble(txtTempGPA.Text);
+                    double thissemcredit = Convert.ToDouble(txtThisCredit.Text);
+                    double origincredit = Convert.ToDouble(txtTempCredit.Text);
+                    string name = txtNameGoal.Text;
+                    goal.dataGridView1.Columns.Add("TotalGPA", name + "想要达到的总绩点");
+                    goal.dataGridView1.Columns.Add("ThisSemGPA", name + "这学期需要得到的绩点");
+                    goal.dataGridView1.Columns.Add("ThisSemGrade", name + "这学期需要得到的成绩");
+                    if (txtGoalGPA.Text != "")
                     {
-                        double goalgpa = Convert.ToDouble(txtGoalGPA.Text);
-                        double resultgpa = Math.Round(((thissemcredit + origincredit) * goalgpa - origingpa * origincredit) / thissemcredit,2);
-                        double resultgrade = Math.Round(100.0 - Math.Sqrt((4.0 - resultgpa) * 1600 / 3),2);
-                        if (resultgpa > 1.0 && resultgpa < 4.0)
+                        if (Convert.ToDouble(txtGoalGPA.Text) < 4.0 && Convert.ToDouble(txtGoalGPA.Text) > 1.0)
                         {
-                            goal.dataGridView1.Rows.Add(goalgpa, resultgpa, resultgrade);
-                            goal.ShowDialog();
+                            double goalgpa = Convert.ToDouble(txtGoalGPA.Text);
+                            double resultgpa = Math.Round(((thissemcredit + origincredit) * goalgpa - origingpa * origincredit) / thissemcredit, 2);
+                            double resultgrade = Math.Round(100.0 - Math.Sqrt((4.0 - resultgpa) * 1600 / 3), 2);
+                            if (resultgpa > 1.0 && resultgpa < 4.0)
+                            {
+                                goal.dataGridView1.Rows.Add(goalgpa, resultgpa, resultgrade);
+                                goal.ShowDialog();
+                            }
+                            else
+                            {
+                                MessageBox.Show("你设定的目标不可能达成！");
+                            }
                         }
                         else
                         {
+                            MessageBox.Show("你输入了错误的目标GPA！");
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 50; i <= 200; i++)
+                        {
+                            double goalgpa = i * 0.02;
+                            double resultgpa = Math.Round(((thissemcredit + origincredit) * goalgpa - origingpa * origincredit) / thissemcredit, 2);
+                            double resultgrade = Math.Round(100.0 - Math.Sqrt((4.0 - resultgpa) * 1600 / 3), 2);
+                            if (resultgpa > 1.0 && resultgpa < 4.0)
+                            {
+                                goal.dataGridView1.Rows.Add(goalgpa, resultgpa, resultgrade);
+                            }
+                        }
+                        if (goal.dataGridView1.RowCount == 0)
+                        {
                             MessageBox.Show("你设定的目标不可能达成！");
                         }
-                    }
-                    else
-                    {
-                        MessageBox.Show("你输入了错误的目标GPA！");
-                    }
-                }
-                else
-                {
-                    for (int i = 50; i <= 200; i++)
-                    {
-                        double goalgpa = i * 0.02;
-                        double resultgpa = Math.Round(((thissemcredit + origincredit) * goalgpa - origingpa * origincredit) / thissemcredit, 2);
-                        double resultgrade = Math.Round(100.0 - Math.Sqrt((4.0 - resultgpa) * 1600 / 3), 2);
-                        if (resultgpa > 1.0 && resultgpa < 4.0)
+                        else
                         {
-                            goal.dataGridView1.Rows.Add(goalgpa, resultgpa, resultgrade);
+                            goal.ShowDialog();
                         }
                     }
-                    if (goal.dataGridView1.RowCount==0)
-                    {
-                        MessageBox.Show("你设定的目标不可能达成！");
-                    }
-                    else
-                    {
-                        goal.ShowDialog();
-                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    btnClearAll_Click(sender, e);
                 }
 
             }
